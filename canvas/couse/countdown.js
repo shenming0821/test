c2f93e685878a8e746df9fa3db8e47539915f3d4
shenming <ming.shen@51auto.com>
2016-08-25 13:36:49 +0800

    create canvs

---
+++ canvas/couse/countdown.js
@@ -0,0 +1,36 @@
+var WINDOW_WIDTH = 1024;
+var WINDOW_HEIGHT = 768;
+window.onload = function () {
+
+    var canvas = document.getElementById('canvas');
+    var context = canvas.getContext('2d');
+
+    canvas.width = WINDOW_WIDTH;
+    canvas.height = WINDOW_HEIGHT;
+
+    render(context)
+}
+
+function render(cxt) {
+    var hours = 12;
+    var minutes = 34;
+    var second = 56;
+
+    renderDigit(0, 0, parseInt(hours / 10), cxt)
+}
+
+function renderDigit(x, y, num, cxt) {
+    cxt.fillStyle = "rgb(0,102,153)";
+
+    for (var i = 0; i < digit[num].length; i++) {
+        for (var j = 0; j < digit[num][i].length; j++) {
+            if (digit[num][i][j] == 1) {
+                cxt.beginPath();
+                
+                ctx.closePath();
+            }
+
+        }
+
+    }
+}
